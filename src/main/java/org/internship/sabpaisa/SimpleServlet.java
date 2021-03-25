ec792e949edd043cbc8f2e81326d620336fa6f80
anand-rathore <av44314@gmail.com>
2021-03-25 12:49:42 +0530

    first commit

---
+++ src/main/java/org/internship/sabpaisa/SimpleServlet.java
@@ -0,0 +1,28 @@
+package org.internship.sabpaisa;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("servlet invoked");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>This message is from servlet</h1>");
+	}
+
+}
